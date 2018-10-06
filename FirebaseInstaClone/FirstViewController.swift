226de9ba31365cc9b5a975358a6794fb7f1b4b41
Atil Samancioglu <samancioglu.atil@gmail.com>
2018-10-06 11:18:17 +0300

    Initial Commit

---
+++ FirebaseInstaClone/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  FirebaseInstaClone
+//
+//  Created by Atil Samancioglu on 6.10.2018.
+//  Copyright © 2018 Atil Samancioglu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ FirebaseInstaClone/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FirebaseInstaClone
+//
+//  Created by Atil Samancioglu on 6.10.2018.
+//  Copyright © 2018 Atil Samancioglu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
